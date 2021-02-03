86dafb3ea5ae820b3583db6e2236fa03d1204a08
claudio <futuro2020es@gmail.com>
2021-02-03 14:57:49 -0300

    ...até a tela da calculadora

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(Calculator,document.getElementById('root'));
+reportWebVitals();
+
---
+++ src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import React, {Component}from 'react'
+import './Calculator.css'
+
+ export default class Calculator extends Component{
+
+    render(){
+        return(
+            <div className="calculator">
+
+            <h1>Calculadora</h1>
+            </div>
+        )
+    }
+
+}
